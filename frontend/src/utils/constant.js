3d17f0910b074bf0b353509416d69a8ce4902b0e
RiteshPanchal279 <riteshpanchal279@gmail.com>
2025-03-09 12:26:14 +0530

    constant change

--- frontend/src/utils/constant.js
+++ frontend/src/utils/constant.js
@@ -1,2 +1,2 @@
-export const USER_API_END_POINT="http://localhost:3000/user";
-export const NOTE_API_END_POINT="http://localhost:3000/note";
+export const USER_API_END_POINT="https://note-app-backend-delta.vercel.app/user";
+export const NOTE_API_END_POINT="https://note-app-backend-delta.vercel.app/note";
